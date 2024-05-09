423bf2a7f450c7148b9372e5e9e12abca056aa9f
Pranav Jaju <100665512+PranavJaju@users.noreply.github.com>
2024-05-09 23:48:25 +0530

    Update assignment4.cpp

--- LP- II/assignment4.cpp
+++ LP- II/assignment4.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 int cnt = 0;
 
-void printBoard(const vector<int>& board) {
+void printBoard(const vector<vector<int>>board) {
     int n = board.size();
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
-            if (board[j] == i)
+            if (board[i][j] == 1)
                 cout << "Q ";
             else
                 cout << ". ";
@@ -20,30 +20,30 @@ void printBoard(const vector<int>& board) {
     cout << endl;
 }
 
-bool isSafe(const vector<int>& board, int col, int row) {
+bool isSafe(int row, int col, const vector<vector<int>> &board) {
     cnt++;
     // Check this row if any queen already exists...
     for (int i = 0; i < col; ++i) {
-        if (board[i] == row)
+        if (board[row][i] == 1)
             return false;
     }
 
     //check for upper left diagonal
-    for (int i = col, j = row; i >= 0 && j >= 0; --i, --j) {
-        if (board[i] == j)
+    for (int i = row, j = col; i >= 0 && j >= 0; --i, --j) {
+        if (board[i][j] == 1)
             return false;
     }
 
     //check for lower left diagonal
-    for (int i = col, j = row; i >= 0 && j < board.size(); --i, ++j) {
-        if (board[i] == j)
+    for (int i = row, j = col; j >= 0 && i < board.size(); i++, j--) {
+        if (board[i][j] == 1)
             return false;
     }
     
     return true;
 }
 
-void solveNQueensUtil(int n, int col, vector<int>& board, int& count) {
+void solveNQueensUtil(int n, int col,vector<vector<int>>board, int& count) {
     if (col == n) {
         ++count;
         cout << "Solution " << count << ":\n";
@@ -52,16 +52,16 @@ void solveNQueensUtil(int n, int col, vector<int>& board, int& count) {
     }
 
     for (int row = 0; row < n; ++row) {
-        if (isSafe(board, col, row)) {
-            board[col] = row;
+        if (isSafe(row,col,board)) {
+            board[row][col] = 1;
             solveNQueensUtil(n, col + 1, board, count);
-            board[col] = -1; // backtrack
+            board[row][col] = 0; // backtrack
         }
     }
 }
 
 int solveNQueens(int n) {
-    vector<int> board(n, -1);
+    vector<vector<int>>board(n,vector<int>(n,0));
     int count = 0;
     solveNQueensUtil(n, 0, board, count);
     return count;
